2dee528eec82fa6528c117fe9dff516349113c9b
Dung34 <2k3dungx@gmail.com>
2025-03-13 11:21:13 +0700

    Init FE + add Footer

---
+++ front-end/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from './component/footer/footer'
+
+const App = () => {
+  return (
+    <div>
+      <body>
+        <h1 class="text-3xl font-bold ">
+          Hello world!
+        </h1>
+        <Footer />
+      </body>
+    </div>
+  )
+}
+
+export default App
--- frontend/src/components/About.js
+++
@@ -1,3 +0,0 @@
-export default function About() {
-  return <div style={{ padding: 10 }}></div>;
-}
