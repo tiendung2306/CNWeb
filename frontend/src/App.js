0783ea64fa6d0be37f51bf0b9053208397df0e25
Dung34 <2k3dungx@gmail.com>
2025-03-15 17:24:32 +0700

    Add banner home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Headers from './components/Headers';
+import Home from './page/home/Home';
 
 function App() {
   return (
     <div className="App">
       <Headers />
+      <Home />
     </div>
   );
 }
