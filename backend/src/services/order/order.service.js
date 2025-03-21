7a3af53452fcd4929e3d30f9852ee08ef05dfc65
tiendung2306 <109841830+tiendung2306@users.noreply.github.com>
2025-03-21 16:21:01 +0700

    add payment api and fix order api

--- backend/src/services/order/order.service.js
+++ backend/src/services/order/order.service.js
@@ -1,4 +1,4 @@
-const {Order} = require("../../models");
+const { Order } = require("../../models");
 const userService = require("../user/user.service");
 const orderItemService = require("../orderitem/orderitem.service");
 
@@ -12,14 +12,14 @@ const createOrder = async (orderData) => {
     const newOrderItems = await Promise.all(orderItems.map(async (orderItem) => {
         return orderItemService.createOrderItem(newOrder.id, orderItem);
     }));
-    return {order: newOrder, orderItems: newOrderItems};
+    return { order: newOrder, orderItems: newOrderItems };
 }
 
 const getAllOrders = async () => {
     const orders = await Order.findAll();
     const ordersWithItems = await Promise.all(orders.map(async (order) => {
         const orderItems = await orderItemService.getOrderItemsByOrderId(order.id);
-        return {order, orderItems};
+        return { order, orderItems };
     }
     ));
     return ordersWithItems;
@@ -27,30 +27,47 @@ const getAllOrders = async () => {
 
 const getOrderById = async (id) => {
     const order = await Order.findByPk(id);
-    if(!order) {
+    if (!order) {
         throw new Error('Order not found');
     }
     const orderWithItems = await orderItemService.getOrderItemsByOrderId(id);
-    return {order, orderItems: orderWithItems};
+    return { order, orderItems: orderWithItems };
+}
+
+const getOrderByUserId = async (id) => {
+    const orders = await Order.findAll({
+        where: {
+            userId: id
+        }
+    });
+    const ordersWithItems = await Promise.all(orders.map(async (order) => {
+        const orderItems = await orderItemService.getOrderItemsByOrderId(order.id);
+        return { order, orderItems };
+    }));
+    return ordersWithItems;
 }
 
 const updateOrderById = async (id, orderData) => {
     const { userId, totalAmount, status, orderDate, deliveryAddress, orderItems } = orderData;
-    const user = await userService.findById(userId);
-    if (!user) {
-        throw new Error('User not found');
+    if (userId) {
+        const user = await userService.findById(userId);
+        if (!user) {
+            throw new Error('User not found');
+        }
     }
     const order = await Order.findByPk(id);
     if (!order) {
         throw new Error('Order not found');
     }
-    await orderItemService.deleteOrderItemsByOrderId(id);
     await order.update(orderData);
-    const newOrderItems = await Promise.all(orderItems.map(async (orderItem) => {
-        return orderItemService.createOrderItem(order.id, orderItem);
-    }));
-    
-    return {order, orderItems: newOrderItems};
+    let newOrderItems = await orderItemService.getOrderItemsByOrderId(id);
+    if (orderItems) {
+        await orderItemService.deleteOrderItemsByOrderId(id);
+        newOrderItems = await Promise.all(orderItems.map(async (orderItem) => {
+            return orderItemService.createOrderItem(order.id, orderItem);
+        }));
+    }
+    return { order, orderItems: newOrderItems };
 }
 
 const deleteOrderById = async (id) => {
@@ -67,6 +84,7 @@ module.exports = {
     createOrder,
     getAllOrders,
     getOrderById,
+    getOrderByUserId,
     updateOrderById,
     deleteOrderById,
 };
